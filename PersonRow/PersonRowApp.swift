25606847ff9a6038c6d899dfc4c174132fd01343
FloDev23 <f.fraccastoro89@gmail.com>
2023-02-14 18:50:41 +0100

    Initial Commit

---
+++ PersonRow/PersonRowApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonRowApp.swift
+//  PersonRow
+//
+//  Created by Floriano Fraccastoro on 14/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PersonRowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
